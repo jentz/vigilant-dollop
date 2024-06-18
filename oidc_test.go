b42639cc2e99fabba9afb6991817e03165531967
Mark Jentz <1196441+jentz@users.noreply.github.com>
2024-06-18 17:03:03 +0200

    chore: fmt code (#30)

--- oidc_test.go
+++ oidc_test.go
@@ -6,26 +6,26 @@ import (
 
 func TestGenerateCodeVerifier(t *testing.T) {
 	var tests = []struct {
-		name string
+		name          string
 		generateBytes int
-		wantBytes int
-		wantPanic bool
+		wantBytes     int
+		wantPanic     bool
 	}{
-		{ "24 bytes random, string too short", 24, 32, true },
-		{ "32 bytes random, 43 bytes string", 32, 43, false },
-		{ "64 bytes random, 86 bytes string", 64, 86, false },
-		{ "96 bytes random, 128 bytes string", 96, 128, false },
-		{ "128 bytes random, string too long", 128, 152, true },
+		{"24 bytes random, string too short", 24, 32, true},
+		{"32 bytes random, 43 bytes string", 32, 43, false},
+		{"64 bytes random, 86 bytes string", 64, 86, false},
+		{"96 bytes random, 128 bytes string", 96, 128, false},
+		{"128 bytes random, string too long", 128, 152, true},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-            defer func() {
-                r := recover()
-                if (r != nil) != tt.wantPanic {
-                    t.Errorf("generateCodeVerifier() recover = %v, wantPanic = %v", r, tt.wantPanic)
-                }
-            }()
+			defer func() {
+				r := recover()
+				if (r != nil) != tt.wantPanic {
+					t.Errorf("generateCodeVerifier() recover = %v, wantPanic = %v", r, tt.wantPanic)
+				}
+			}()
 			gotBytes := len(generateCodeVerifier(tt.generateBytes))
 			if gotBytes != tt.wantBytes {
 				t.Errorf("err got %v, want %v", gotBytes, tt.wantBytes)
