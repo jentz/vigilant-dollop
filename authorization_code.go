db09cf155761849749d90618fceff627ce116452
Remco Jansen <remcojansen@gmail.com>
2024-06-19 19:08:08 +0200

    adds custom arguments for authorization_code (#33)

--- authorization_code.go
+++ authorization_code.go
@@ -8,12 +8,13 @@ type AuthorizationCodeFlow struct {
 type AuthorizationCodeFlowConfig struct {
 	Scopes      string
 	CallbackURI string
-	PKCE        bool
+	CustomArgs  CustomArgs
+	PKCE		bool
 }
 
 func (c *AuthorizationCodeFlow) Run() error {
 	c.Config.DiscoverEndpoints()
 
-	HandleOpenIDFlow(c.Config.ClientID, c.Config.ClientSecret, c.FlowConfig.Scopes, "http://localhost:9555/callback", c.Config.DiscoveryEndpoint, c.Config.AuthorizationEndpoint, c.Config.TokenEndpoint, c.FlowConfig.PKCE)
+	HandleOpenIDFlow(c.Config.ClientID, c.Config.ClientSecret, c.FlowConfig.Scopes, "http://localhost:9555/callback", c.Config.DiscoveryEndpoint, c.Config.AuthorizationEndpoint, c.Config.TokenEndpoint, c.FlowConfig.CustomArgs, c.FlowConfig.PKCE)
 	return nil
 }
